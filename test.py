dae8f54baabc297e2692e2508ac700d55258e013
YijunXie <yijun.xie@uwaterloo.ca>
2020-05-27 12:04:24 -0400

    initialize function file and test file

---
+++ test.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import sklearn
+from sklearn.datasets import load_boston
+from sklearn.model_selection import train_test_split
+
+boston = load_boston()
+bos = pd.DataFrame(boston.data)
+
+bos.columns = boston.feature_names
+
+X = bos
+y = boston.target
+
+X_train, X_test, y_tarin, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
