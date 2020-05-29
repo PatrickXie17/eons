e40080e2966212ac1fea81c373bbd0a666427a3e
YijunXie <yijun.xie@uwaterloo.ca>
2020-05-28 23:34:48 -0400

    add the linear regression case for eons

--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sklearn
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
+import numpy as np
 
 boston = load_boston()
 bos = pd.DataFrame(boston.data)
@@ -11,5 +12,6 @@ bos.columns = boston.feature_names
 X = bos
 y = boston.target
 
-X_train, X_test, y_tarin, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
 
+sp
