08efd262fbd7d5da98228bfcd08d65642fc01259
YijunXie <yijun.xie@uwaterloo.ca>
2020-05-29 07:16:45 -0400

    add weight shrinkage

--- funs.py
+++ funs.py
@@ -2,76 +2,77 @@ import numpy as np
 import scipy
 from sklearn.linear_model import LinearRegression
 
-class Eons():
-    def __init__(self):
-        self
 
-    def l2_loss():
-        """
-        This function calculate the L2 loss of two arrays
-        :param d1: The first np array
-        :param d2: The second np array
-        :return: MSE
-        """
-        def tf(d1, d2):
-            return np.mean((np.array(d1) - np.array(d2)) ** 2)
+def l2_loss():
+    """
+    This function calculate the L2 loss of two arrays
+    :param d1: The first np array
+    :param d2: The second np array
+    :return: MSE
+    """
 
-        return tf
+    def tf(d1, d2):
+        return np.mean((np.array(d1) - np.array(d2)) ** 2)
 
-    def feed_forward_regression(self, weight, *args):
-        """
-        This is the feed forward step of a neural network
-        :param X: Input data. Each column is a variable and each row is an observation.
-        :param y: Target.
-        :param weight: Weight of each variable.
-        :param loss_fun: Loss metric to evaluate the loss.
-        :return: Total forward loss.
-        """
-        X_train = args[0]
-        X_test = args[1]
-        y_train = args[2]
-        y_test = args[3]
-        loss_fun = args[4]
-        # self-normalization for the weights
-        weight = weight / np.sqrt(np.sum(weight ** 2))
+    return tf
 
-        # projection onto weights
-        projs = np.dot(X_train, weight)
-        projs = projs.reshape(projs.shape[0],1)
 
-        # calculate forward loss
-        tm = LinearRegression()
-        tm.fit(projs, y_train)
+def feed_forward_regression(weight, *args):
+    """
+    This is the feed forward step of a neural network
+    :param X: Input data. Each column is a variable and each row is an observation.
+    :param y: Target.
+    :param weight: Weight of each variable.
+    :param loss_fun: Loss metric to evaluate the loss.
+    :return: Total forward loss.
+    """
+    X_train = args[0]
+    X_test = args[1]
+    y_train = args[2]
+    y_test = args[3]
+    loss_fun = args[4]
+    # self-normalization for the weights
+    weight = weight / np.sqrt(np.sum(weight ** 2))
 
-        test_proj = np.dot(X_test, weight)
-        test_proj = test_proj.reshape(-1,1)
-        yhat = tm.predict(test_proj)
+    # projection onto weights
+    projs = np.dot(X_train, weight)
+    projs = projs.reshape(projs.shape[0], 1)
 
-        t_loss = loss_fun(yhat, y_test)
+    # calculate forward loss
+    tm = LinearRegression()
+    tm.fit(projs, y_train)
 
-        return t_loss
+    test_proj = np.dot(X_test, weight)
+    test_proj = test_proj.reshape(-1, 1)
+    yhat = tm.predict(test_proj)
 
-    def train_regression(self, X_train, X_test, y_train, y_test, loss_fun, thred, max_iter):
-        diff = 1000
-        counter = 0
+    t_loss = loss_fun(yhat, y_test)
 
-        # initialize weight
-        weight = np.random.normal(0, 1, X_train.shape[1])
-        weight = weight / np.sqrt(np.sum(weight ** 2))
+    return t_loss
 
-        # construct boundaries for L-BFGS-B optimizer
-        bounds = []
-        for i in range(len(weight)):
-            bounds.append((-1, 1))
 
-        # iterate until converge
-        while (diff > thred) and (counter < max_iter):
-            o_weight = weight
-            k = scipy.optimize.fmin_l_bfgs_b(self.feed_forward_regression, x0=weight,
-                                             args=(X_train, X_test, y_train, y_test, loss_fun),
-                                             bounds=bounds, approx_grad = True)
-            weight = k[0]
-            diff = np.sum((weight - o_weight)**2)
+def train_regression(X_train, X_test, y_train, y_test, loss_fun, thred, max_iter, q):
+    diff = 1000
+    counter = 0
 
-        return weight
+    # initialize weight
+    weight = np.random.normal(0, 1, X_train.shape[1])
+    weight = weight / np.sqrt(np.sum(weight ** 2))
 
+    # construct boundaries for L-BFGS-B optimizer
+    bounds = []
+    for i in range(len(weight)):
+        bounds.append((-1, 1))
+
+    # iterate until converge
+    while (diff > thred) and (counter < max_iter):
+        o_weight = weight
+        k = scipy.optimize.fmin_l_bfgs_b(feed_forward_regression, x0=weight,
+                                         args=(X_train, X_test, y_train, y_test, loss_fun),
+                                         bounds=bounds, approx_grad=True)
+        weight = k[0]
+        weight[weight < q] = 0
+        diff = np.sum((weight - o_weight) ** 2)
+
+    weight = weight / np.sqrt(np.sum(weight ** 2))
+    return weight
--- test.py
+++ test.py
@@ -2,6 +2,7 @@ import pandas as pd
 import sklearn
 from sklearn.datasets import load_boston
 from sklearn.model_selection import train_test_split
+from sklearn import preprocessing
 import funs
 
 boston = load_boston()
@@ -10,9 +11,10 @@ bos = pd.DataFrame(boston.data)
 bos.columns = boston.feature_names
 
 X = bos
+X = preprocessing.scale(X)
 y = boston.target
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
 
-tn = funs.Eons()
-kw = tn.train_regression(X_train, X_test, y_train, y_test, funs.Eons.l2_loss(), thred = 0.1, max_iter = 10)
+
+kw = train_regression(X_train, X_test, y_train, y_test, l2_loss(), thred = 0.01, max_iter = 100, q = 0.01)
